d21ae92174080d2596cab13ffafda837cb09ae1c
lukas <lukas@localhost.localdomain>
2023-10-23 01:33:57 -0300

    se arreglaron dependencias

--- autobspwn.sh
+++ autobspwn.sh
@@ -13,9 +13,9 @@ mkdir install
 cd install
 
 echo  -ne "Clonando Bspwm"
-git clone https://github.com/baskerville/bspwm.github
+git clone https://github.com/baskerville/bspwm.git
 echo -ne "Clonand sxhdk"
-git clone https://github.com/baskerville/sxhkd.github
+git clone https://github.com/baskerville/sxhkd.git
 
 echo -ne "Compilando Bspwm"
 cd bspwm/
@@ -39,7 +39,7 @@ echo -ne "Instalando dependencias"
 
 
 # hay una dependencia que da problema hay que sacarla
-sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libnl-genl-3-dev
 
 echo -ne "Clonando polybar"
 cd $path
@@ -70,6 +70,7 @@ echo -ne "Instalando lsd"
 wget "https://github.com/lsd-rs/lsd/releases/download/v1.0.0/lsd_1.0.0_amd64.deb"
 
 echo -ne "Instalando zstd para crear nuevo .deb"
+sudo apt install zstd
 set -e
 ar x "lsd_1.0.0_amd64.deb"
 zstd -d < control.tar.zst | xz > control.tar.xz
