62fc4454b734f0fd1e7bfef65d2802850db8d1e8
lukas <lukas@localhost.localdomain>
2023-10-23 02:40:45 -0300

    instalacion de picom

--- autobspwn.sh
+++ autobspwn.sh
@@ -42,7 +42,7 @@ echo -ne "Instalando dependencias"
 sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libnl-genl-3-dev
 
 echo -ne "Clonando polybar"
-cd $path
+cd $path/install
 git clone --recursive https://github.com/polybar/polybar
 cd polybar/
 mkdir build
@@ -55,6 +55,20 @@ sudo make install
 
 echo -ne "Copiando Archivos de Configuracion la polybar"
 cp -r $path/polybar/ ~/.config/polybar
+
+echo -ne "Instalando dependecias\n"
+sudo apt update
+sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libuv1-dev
+
+echo -ne "Instalando picom\n"
+cd $path/install
+git clone https://github.com/ibhagwan/picom.git
+cd picom/
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
 
 echo -ne "Instalando rofi"
 sudo apt install rofi
