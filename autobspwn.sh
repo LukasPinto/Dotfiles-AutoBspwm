63a90e985fa3fa865a4359d650996b80525a7dcf
lukas <lukas@localhost.localdomain>
2023-10-24 22:42:12 -0300

    fix arg

--- autobspwn.sh
+++ autobspwn.sh
@@ -3,7 +3,7 @@
 
 echo -ne "Actualizando paquetes \n"
 
-if [["$user"]];then
+if [[ "$user" ]];then
   user="$1"
 
   sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev -y
